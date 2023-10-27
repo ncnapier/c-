328d3f1ffc7d47f6d5a3975732efd79ef7578596
Nathaniel C Napier <nattydevs@gmail.com>
2023-10-27 09:12:53 -0500

    Create formatAndDisplay.cs

---
+++ formatAndDisplay.cs
@@ -0,0 +1,5 @@
+string outPut = "View English output:";
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($"{outPut} \n\tc:\\Exercise\\{projectName}\\data.txt\n\n{russianMessage}:\n\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt");
