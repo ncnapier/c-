00bc142121c66b0fc315bcee0bb38819c3a892fd
Nathaniel C Napier <nattydevs@gmail.com>
2023-11-02 13:49:48 -0500

    Create subscriptionDiscount.cs

---
+++ subscriptionDiscount.cs
@@ -0,0 +1,30 @@
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+if ( daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+    Console.WriteLine("Your subscription will expire soon.\n Renew now!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\n Renew now and save {discountPercentage}%!");
+    
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"your subscription expires within a day!\n Renew now and save {discountPercentage}%!");
+    
+}
+else if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else
+{
+    Console.WriteLine(" ");
+}
