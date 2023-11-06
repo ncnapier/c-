755234d9880ecc9564a8a97abdaeea43019252b8
Nathaniel C Napier <nattydevs@gmail.com>
2023-11-06 10:19:43 -0600

    Create fraudulentOrdersStartWithB.cs

---
+++ fraudulentOrdersStartWithB.cs
@@ -0,0 +1,9 @@
+string[] list = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string order in list)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
+}
