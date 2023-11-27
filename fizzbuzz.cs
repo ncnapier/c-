44b0861ae566ff512fd7fba33bbe74e70908cdab
Nathaniel C Napier <nattydevs@gmail.com>
2023-11-27 14:44:08 -0600

    clean up fizzbuzz.cs

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,16 +1,15 @@
-for(int i = 0; i < 101; i++)
+for(int i = 1; i < 101; i++)
 {
     if (i % 3 == 0 && i % 5 == 0) 
-{
-    Console.WriteLine("FizzBuz");
-}else if (i % 3 == 0)
-{
-    Console.WriteLine("Fizz");
-}else if ( i % 5 == 0)
-{
-    Console.WriteLine("Buzz");
-}else
-{
+
+    Console.WriteLine($"{i}-FizzBuz");
+else if (i % 3 == 0)
+
+    Console.WriteLine($"{i}-Fizz");
+else if ( i % 5 == 0)
+
+    Console.WriteLine($"{i}-Buzz");
+else
+
     Console.WriteLine(i);
-}
 }
